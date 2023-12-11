826e0d7e1d6dddbabaf83470ae2ac8f939da3ad8
jina95 <spzn02@gmail.com>
2023-12-11 00:22:32 +0000

    ing

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const path = require("path")
 
 const app = express()
-const port = process.env.PORT || "8000"
+const port = process.env.PORT || "8080"
 
 app.set("views", path.join(__dirname, "views"))
 app.set("view engine", "pug")
